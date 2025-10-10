fc10ae58028d13d54a534de763ded947659131c1
sharnam20 <shahsharnam2006@gmail.com>
2025-10-10 07:58:07 +0530

    Add Practicals 5, 6, 7 - Calculator, Todo App, and Sidebar Navigation

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Practical3 from './components/Practical3'
+// import Greet from './components/greetings'
+import { Component } from 'react'
+import Greet from './components/greetings'
+import Practical4 from './components/Practical4'
+import Practical5 from './components/Practical5'
+import Practical6 from './components/Practical6'
+import Practical7 from './components/Practical7'
+// import Welcome from './components/welcome'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Practical3 /> */}
+      {/* <Greet /> */}
+      {/* <Practical4 /> */}
+      {/* <Practical5 /> */}
+      {/* <Practical6 /> */}
+      <Practical7 />
+    </>
+  )
+}
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <div>
+//         <Greet name="CSE" institute="CSPIT"/>
+//         <Greet name="IT" institute="DEPSTAR"/>
+//       </div>
+//     )
+//   }
+// }
+
+export default App
